c684e30abf3b34d0f750aa96923bff2b9af2c86e
ETBox <13318972468@126.com>
2019-11-03 21:16:10 +0800

    feat: get contributors

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import requests
-r = requests.get('https://api.github.com/repos/psf/requests')
-print(r.json()["description"])
+
+# Detailed representations
+repo_url = 'https://api.github.com/repos/psf/requests'
+request_headers = {'Accept': 'application/vnd.github.v3+json'}
+# response = requests.get(repo_url, headers=request_headers)
+
+# # List all topics for a repository
+# request_headers = {'Accept': 'application/vnd.github.mercy-preview+json'}
+# response = requests.get(repo_url+'/topics', headers=request_headers)
+
+# List contributors
+response = requests.get(repo_url+'/contributors', headers=request_headers)
+
+print(response.json())
+# print(response.headers)
