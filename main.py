f05d65cea4af815c70116cc1db18a3d3c71665ac
ETBox <13318972468@126.com>
2019-11-03 21:26:27 +0800

    feat: get last page of contributors

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ request_headers = {'Accept': 'application/vnd.github.v3+json'}
 
 # List contributors
 response = requests.get(repo_url+'/contributors', headers=request_headers)
+# Link →<https://api.github.com/repositories/1362490/contributors?page=2>; rel="next", <https://api.github.com/repositories/1362490/contributors?page=14>; rel="last"
+
+# # Set to 1 or true to include anonymous contributors in results.
+# response = requests.get(repo_url+'/contributors?anon=1',
+#                         headers=request_headers)
+# #Link →<https://api.github.com/repositories/1362490/contributors?anon=1&page=2>; rel="next", <https://api.github.com/repositories/1362490/contributors?anon=1&page=22>; rel="last"
 
 print(response.json())
 # print(response.headers)
